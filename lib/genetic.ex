d29f43f5faa02d3d9e376573f14414f5a66f4302
b0a04gl <balaji.foo404@gmail.com>
2023-10-27 00:00:35 +0530

    step#2 : pipeline/evaluate to refine population

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -8,9 +8,29 @@ defmodule Genetic do
 
 def run(problem, opts \\ []) do
     population = initialize(&problem.genotype/0)
+    population
+    |> evolve(problem, 0, opts)
+
   end
 
+  def evaluate(population, fitness_function, opts \\ []) do
+      population
+    |> Enum.map(
+        fn chromosome ->
+          fitness = fitness_function.(chromosome)
+          age = chromosome.age + 1
+          %Chromosome{chromosome | fitness: fitness, age: age}
+          end
+      )
+    |> Enum.sort_by(fitness_function, &>=/2)
+  end
 
+  def evolve(population, problem, generation, opts \\ []) do
+    population = evaluate(population, &problem.fitness_function/1, opts)
+    best = hd(population)
+    IO.write("\rCurrent optimal : #{best.fitness}")
+
+  end
 
 
 end
