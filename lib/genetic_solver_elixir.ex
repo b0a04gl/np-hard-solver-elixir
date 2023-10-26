b4fe7db4316a1d3230cbcb2a1be756d35d61f9fb
b0a04gl <Balaji.Arumugam@walmart.com>
2023-10-26 23:04:07 +0530

    base mix setup

---
+++ lib/genetic_solver_elixir.ex
@@ -0,0 +1,18 @@
+defmodule GeneticSolverElixir do
+  @moduledoc """
+  Documentation for `GeneticSolverElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeneticSolverElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/genetic_solver_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule GeneticSolverElixirTest do
+  use ExUnit.Case
+  doctest GeneticSolverElixir
+
+  test "greets the world" do
+    assert GeneticSolverElixir.hello() == :world
+  end
+end
