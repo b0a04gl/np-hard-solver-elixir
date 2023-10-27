25b03396a6f1167f8322bbf9e61451326d5694b2
b0a04gl <balaji.foo404@gmail.com>
2023-10-27 07:20:44 +0530

    realigned project structure

--- lib/nqueens.ex
+++ lib/nqueens.ex
@@ -36,3 +36,9 @@ soln = Genetic.run(NQueens)
 
 IO.write("\n")
 IO.inspect(soln)
+
+# Use reflection to find the module name
+module_name = Module.concat([Kernel, NQueens])
+
+# Print the module name
+IO.puts("Module name: #{module_name}")
